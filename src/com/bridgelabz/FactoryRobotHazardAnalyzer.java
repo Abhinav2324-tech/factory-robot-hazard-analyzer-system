35476e86e4726875f0d45386a3fc2c3df674d643
Abhinav2324-tech <abhinavgoud2324@gmail.com>
2026-09-01 11:29:11 +0530

    [Abhinav]:Implemented UC1 display static hazard message.

---
+++ src/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+/*
+ * This program displays the name of the Factory Robot Hazard Analyzer
+ * system when the application is executed.
+ */
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
