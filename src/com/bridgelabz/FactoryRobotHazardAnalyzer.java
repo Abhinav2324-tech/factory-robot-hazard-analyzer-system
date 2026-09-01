acd49d08f80d44a2a4a1e4ef44af7cd2abfb0875
Abhinav2324-tech <abhinavgoud2324@gmail.com>
2026-09-01 12:33:07 +0530

    [Abhinav]:Implemented UC4 input validation using conditional logic.

--- src/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -1,8 +1,9 @@
 package com.bridgelabz;
 /*
  * This program accepts robot hazard-related inputs from the user,
- * calculates the hazard risk score using the given formula,
- * and displays the calculated result.
+ * validates the entered values using conditional statements,
+ * calculates the hazard risk score for valid inputs,
+ * and displays an appropriate error message for invalid inputs.
  */
 
 import java.util.Scanner;
@@ -18,19 +19,33 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter the machinery state:");
         String machineryState=input.nextLine();
 
-        double machineRiskFactor=0.0;
-        //Determining the machine risk factor based on machinery state.
-        if(machineryState.equals("Worn")){
-            machineRiskFactor=1.3;
+        if(armPrecision<0.0 || armPrecision>1.0){ // Validate arm precision
+            System.out.println("Error: Arm precision must be 0.0-1.0");
         }
-        else if(machineryState.equals("Faulty")){
-            machineRiskFactor=2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineRiskFactor=3.0;
+        else if(workerDensity<1 || workerDensity>20){ // Validate worker density
+            System.out.println("Error: Worker density must be 1-20");
         }
-        double hazardRisk=((1.0 - armPrecision) * 15.0) + (workerDensity *
-                machineRiskFactor);//Hazard risk formula
-        System.out.println("Hazard Risk Score is: "+hazardRisk);//Printing the hazard risk score
-        input.close();
+        else if(!machineryState.equals("Worn")
+        && (!machineryState.equals("Faulty"))
+        && (!machineryState.equals("Critical"))){ // Validate machinery state
+            System.out.println("Error: Unsupported machinerystate");
+        } else {
+            double machineRiskFactor = 0.0;
+
+
+            //Determining the machine risk factor based on machinery state.
+            if (machineryState.equals("Worn")) {
+                machineRiskFactor = 1.3;
+            } else if (machineryState.equals("Faulty")) {
+                machineRiskFactor = 2.0;
+            } else if (machineryState.equals("Critical")) {
+                machineRiskFactor = 3.0;
+            }
+
+            double hazardRisk = ((1.0 - armPrecision) * 15.0) + (workerDensity *
+                    machineRiskFactor);//Hazard risk formula
+            System.out.println("Hazard Risk Score is: " + hazardRisk);//Printing the hazard risk score
+        }
+            input.close();
     }
 }
