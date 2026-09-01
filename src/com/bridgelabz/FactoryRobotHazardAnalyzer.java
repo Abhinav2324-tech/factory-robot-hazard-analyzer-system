33a3c78e7ab908c3c65af846d1e0426c3e20b8cd
Abhinav2324-tech <abhinavgoud2324@gmail.com>
2026-09-01 12:10:08 +0530

    [Abhinav]:Implemented UC3 calculate hazard risk score.

--- src/com/bridgelabz/FactoryRobotHazardAnalyzer.java
+++ src/com/bridgelabz/FactoryRobotHazardAnalyzer.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
 /*
  * This program accepts robot hazard-related inputs from the user,
- * including arm precision, worker density, and machinery state,
- * and displays the entered values.
+ * calculates the hazard risk score using the given formula,
+ * and displays the calculated result.
  */
+
 import java.util.Scanner;
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        //Taking the input from user.
         System.out.println("Enter the arm precision:");
         double armPrecision=input.nextDouble();
         System.out.println("Enter the worker density:");
@@ -16,9 +18,19 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter the machinery state:");
         String machineryState=input.nextLine();
 
-        System.out.println("Arm Precision: "+armPrecision);
-        System.out.println("Worker Density: "+workerDensity);
-        System.out.println("Machinery State: "+machineryState);
+        double machineRiskFactor=0.0;
+        //Determining the machine risk factor based on machinery state.
+        if(machineryState.equals("Worn")){
+            machineRiskFactor=1.3;
+        }
+        else if(machineryState.equals("Faulty")){
+            machineRiskFactor=2.0;
+        } else if (machineryState.equals("Critical")) {
+            machineRiskFactor=3.0;
+        }
+        double hazardRisk=((1.0 - armPrecision) * 15.0) + (workerDensity *
+                machineRiskFactor);//Hazard risk formula
+        System.out.println("Hazard Risk Score is: "+hazardRisk);//Printing the hazard risk score
         input.close();
     }
 }
